f8b11028f226c636ff9cab32a63a7c2c235dab30
dezoliveira <andresoliveira@protonmail.com>
2024-10-24 21:11:22 -0300

    add: get post data from form

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,7 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        return HttpResponse([
+            {
+                'username': {username},
+                'email': {email},
+                'senha': {senha},
+                'confirmar_senha': {confirmar_senha}
+            }
+        ])
 
 def logar(request):
     return HttpResponse('Você está na página de login')
