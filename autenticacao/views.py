0929906e0594ec34a6815551ad50020cc412aee6
Andres Oliveira <andresoliveira@protonmail.com>
2024-10-30 15:14:51 -0300

    add: User validations and register

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,5 +1,11 @@
+# Django
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+
+# Utils
+from .utils import password_is_valid
 
 # Create your views here.
 def cadastro(request):
@@ -12,6 +18,9 @@ def cadastro(request):
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
 
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+
         return HttpResponse([
             {
                 'username': {username},
@@ -20,6 +29,20 @@ def cadastro(request):
                 'confirmar_senha': {confirmar_senha}
             }
         ])
+    
+    try:
+        user = User.objects.create_user(
+            username=username,
+            password=senha,
+            is_active=False
+        )
+
+        user.save()
+        return redirect('/auth/cadastro')
+
+    except:
+        return redirect('/auth/cadastro')
+        
 
 def logar(request):
     return HttpResponse('Você está na página de login')
